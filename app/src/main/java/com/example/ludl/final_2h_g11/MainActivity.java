016b7ac63054dcc1134da3484899a961edbacde4
ludom171 <44213909+ludom171@users.noreply.github.com>
2019-01-16 18:52:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ludl/final_2h_g11/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ludl.final_2h_g11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
